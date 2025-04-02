4d764e02eec9d2ab048409fbeb3442786a4e7775
Edanur <edanur.rodrigues@edu.ece.fr>
2025-04-02 12:30:07 +0200

    maj user numero de tel bdd

--- src/attraction.sql
+++ src/attraction.sql
@@ -1,61 +1,146 @@
-CREATE TABLE User (
-                      id_user INT PRIMARY KEY AUTO_INCREMENT,
-                      nom VARCHAR(50),
-                      prenom VARCHAR(50),
-                      email VARCHAR(100) UNIQUE,
-                      tel VARCHAR(20),
-                      mdp VARCHAR(255),
-                      role ENUM('client', 'admin') NOT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 02 avr. 2025 à 09:53
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
 
-CREATE TABLE Attraction (
-                            id_attraction INT PRIMARY KEY AUTO_INCREMENT,
-                            nom VARCHAR(100),
-                            type VARCHAR(50),
-                            capacite INT,
-                            duree TIME,
-                            statut ENUM('active', 'inactive') DEFAULT 'active'
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE Reservation (
-                             id_reservation INT PRIMARY KEY AUTO_INCREMENT,
-                             id_user INT,
-                             id_attraction INT,
-                             date_reservation DATE,
-                             statut ENUM('en_attente', 'confirmee', 'annulee') DEFAULT 'en_attente',
-                             FOREIGN KEY (id_user) REFERENCES User(id_user),
-                             FOREIGN KEY (id_attraction) REFERENCES Attraction(id_attraction)
-);
 
-CREATE TABLE Billet (
-                        id_billet INT PRIMARY KEY AUTO_INCREMENT,
-                        id_reservation INT,
-                        statut ENUM('valide', 'utilise', 'expire') DEFAULT 'valide',
-                        FOREIGN KEY (id_reservation) REFERENCES Reservation(id_reservation)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE Paiement (
-                          id_paiement INT PRIMARY KEY AUTO_INCREMENT,
-                          id_reservation INT,
-                          montant DECIMAL(10,2),
-                          statut ENUM('en_attente', 'effectue', 'refuse') DEFAULT 'en_attente',
-                          date_paiement DATE,
-                          FOREIGN KEY (id_reservation) REFERENCES Reservation(id_reservation)
-);
+--
+-- Base de données : `attraction`
+--
 
-CREATE TABLE Historique (
-                            id_historique INT PRIMARY KEY AUTO_INCREMENT,
-                            id_user INT,
-                            id_reservation INT,
-                            date_visite DATE,
-                            FOREIGN KEY (id_user) REFERENCES User(id_user),
-                            FOREIGN KEY (id_reservation) REFERENCES Reservation(id_reservation)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE Reduction (
-                           id_reduction INT PRIMARY KEY AUTO_INCREMENT,
-                           id_user INT,
-                           pourcentage DECIMAL(5,2),
-                           raison VARCHAR(100),
-                           FOREIGN KEY (id_user) REFERENCES User(id_user)
-);
+--
+-- Structure de la table `attraction`
+--
+
+DROP TABLE IF EXISTS `attraction`;
+CREATE TABLE IF NOT EXISTS `attraction` (
+  `id_attraction` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `capacite` int DEFAULT NULL,
+  `duree` time DEFAULT NULL,
+  `statut` enum('active','inactive') DEFAULT 'active',
+  PRIMARY KEY (`id_attraction`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billet`
+--
+
+DROP TABLE IF EXISTS `billet`;
+CREATE TABLE IF NOT EXISTS `billet` (
+  `id_billet` int NOT NULL AUTO_INCREMENT,
+  `id_reservation` int DEFAULT NULL,
+  `statut` enum('valide','utilise','expire') DEFAULT 'valide',
+  PRIMARY KEY (`id_billet`),
+  KEY `id_reservation` (`id_reservation`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `historique`
+--
+
+DROP TABLE IF EXISTS `historique`;
+CREATE TABLE IF NOT EXISTS `historique` (
+  `id_historique` int NOT NULL AUTO_INCREMENT,
+  `id_user` int DEFAULT NULL,
+  `id_reservation` int DEFAULT NULL,
+  `date_visite` date DEFAULT NULL,
+  PRIMARY KEY (`id_historique`),
+  KEY `id_user` (`id_user`),
+  KEY `id_reservation` (`id_reservation`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paiement`
+--
+
+DROP TABLE IF EXISTS `paiement`;
+CREATE TABLE IF NOT EXISTS `paiement` (
+  `id_paiement` int NOT NULL AUTO_INCREMENT,
+  `id_reservation` int DEFAULT NULL,
+  `montant` decimal(10,2) DEFAULT NULL,
+  `statut` enum('en_attente','effectue','refuse') DEFAULT 'en_attente',
+  `date_paiement` date DEFAULT NULL,
+  PRIMARY KEY (`id_paiement`),
+  KEY `id_reservation` (`id_reservation`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reduction`
+--
+
+DROP TABLE IF EXISTS `reduction`;
+CREATE TABLE IF NOT EXISTS `reduction` (
+  `id_reduction` int NOT NULL AUTO_INCREMENT,
+  `id_user` int DEFAULT NULL,
+  `pourcentage` decimal(5,2) DEFAULT NULL,
+  `raison` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_reduction`),
+  KEY `id_user` (`id_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `id_reservation` int NOT NULL AUTO_INCREMENT,
+  `id_user` int DEFAULT NULL,
+  `id_attraction` int DEFAULT NULL,
+  `date_reservation` date DEFAULT NULL,
+  `statut` enum('en_attente','confirmee','annulee') DEFAULT 'en_attente',
+  PRIMARY KEY (`id_reservation`),
+  KEY `id_user` (`id_user`),
+  KEY `id_attraction` (`id_attraction`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `tel` int DEFAULT NULL,
+  `mdp` varchar(255) DEFAULT NULL,
+  `role` enum('client','admin') NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
